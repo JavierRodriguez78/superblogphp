9006ee485a3f44d43889d68d168134132cbfb12d
Xavi <xavi@geekshubs.com>
2019-06-10 10:52:38 +0200

    finish blog-builder

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,16 +1,15 @@
 <?php
 namespace App\controllers;
-use App\DoctrineManager;
+use App\services\PostsService;
 use App\models\entities\Post;
 
 use Kint;
 class HomeController extends Controller
 {
     public function index(){
-        $doctrineManager = $this->container->get(DoctrineManager::class);
-        $repository = $doctrineManager->em->getRepository(Post::class);
-        $posts=$repository->findAll();
+        $PostsService = $this->container->get(PostsService::class);
+        $posts= $PostsService->getPosts();
         Kint::dump($posts);
-        $this->viewManager->renderTemplate("index.view.html");
+        $this->viewManager->renderTemplate("index.view.html",['posts'=>$posts]);
     }
 }
