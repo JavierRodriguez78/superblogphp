43fb3499019fc0c6fa23e5e1975b8752cd8cc2ae
Xavi <xavi@geekshubs.com>
2019-06-12 12:12:16 +0200

    added bootstrap and javascript function

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-alert("hola mundo");
+var deleteModal = $('#deleteModal');
+
+
+
+function deleteMsg(blog, id){
+    $(this).click(function(event){
+        event.preventDefault();
+    });
+
+    deleteModal.modal('show');
+    $('.modal-body').text(blog);
+    $('.modal-footer .btn-danger').attr("id",id);
+}
+
+function confirmDelete(){
+    alert("pulsado");
+   
+        var id = $(this).attr("id");
+        console.log(id);
+        $(this).onclick="location.href='/delete-post/"+id+"'";
+
+    }
+
