f05b572dc375209f60427aba7afbd58e43b242de
Xavi <xavi@geekshubs.com>
2019-06-13 11:03:03 +0200

    include suite test

---
+++ test/Unit/firstTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace AppTest\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class firstTest extends TestCase{
+
+    /**
+     *  @test
+     */
+    public function testFirst(){
+        $variable = false;
+
+        $this->assertTrue($variable);
+    }
+}
